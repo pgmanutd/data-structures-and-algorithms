02f61909fefb420c3cb8aa4eebf1cfc16e77bb1a
pgmanutd <prashantmanunited@gmail.com>
2019-04-21 02:20:10 +0530

    chore: remove typecheck npm command from husky

--- .huskyrc.js
+++ .huskyrc.js
@@ -2,7 +2,7 @@ const tasks = array => array.join(' && ');
 
 module.exports = {
   hooks: {
-    'pre-commit': tasks(['npm run typecheck', 'lint-staged']),
+    'pre-commit': tasks(['lint-staged']),
     'commit-msg': tasks(['commitlint -E HUSKY_GIT_PARAMS']),
     'post-merge': tasks(['node ./scripts/husky/post-merge']),
   },
